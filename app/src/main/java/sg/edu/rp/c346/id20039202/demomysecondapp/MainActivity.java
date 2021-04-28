861ac904b1caf2ff3918e3978136ea38454dfeab
yinMinz123 <20039202@myrp.edu.sg>
2021-04-28 10:39:03 +0800

    Second App Demo

---
+++ app/src/main/java/sg/edu/rp/c346/id20039202/demomysecondapp/MainActivity.java
@@ -0,0 +1,17 @@
+package sg.edu.rp.c346.id20039202.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //This is a new line
+    }
+}
